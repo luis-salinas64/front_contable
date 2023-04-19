90c99ae9f40e7d773e8edb8c7fb7b3a5a2bd99b0
luis-salinas64 <salinasluis6408@gmail.com>
2023-04-18 22:32:47 -0300

    Ingresos

--- src/pages/ingresos.js
+++ src/pages/ingresos.js
@@ -5,30 +5,31 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Select from '@mui/material/Select'; // importar Select de MUI
+import MenuItem from '@mui/material/MenuItem'; // importar MenuItem de MUI
+
 
 const MyContainer = styled(Container)({
-  backgroundColor: '#C4E0E5', 
-  position:'absolute',
-  height:'720px',
-  top:'101px',
-  left:'171px',
+  backgroundColor: '#C4E0E5',
+  position: 'absolute',
+  // height: '720px',
+  top: '101px',
+  left: '171px',
   display: 'flex',
   flexDirection: 'column',
   // justifyContent: 'center',
   // alignItems: 'center',
-  margin:0,
-  
+  margin: 0,
+
 });
 
 const MyForm = styled('form')({
   display: 'flex',
   flexDirection: 'column',
-  
+
 });
 
 const MyRow = styled('div')({
@@ -38,19 +39,13 @@ const MyRow = styled('div')({
 });
 
 const MyField = styled(TextField)({
+  width:"500px",
   margin: '12px',
   '& .MuiInputBase-input': {
     padding: '10px',
     backgroundColor: '#FFFFFF',
+    color: 'black'
   },
-});
-
-const MyFormControl = styled(FormControl)({
-  margin: '8px',
-});
-
-const MyInputLabel = styled(InputLabel)({
-  marginRight: '8px',
 });
 
 const MyButtonRow = styled(MyRow)({
@@ -71,7 +66,15 @@ const MyDatePicker = styled(DatePicker)({
     borderRadius: '4px',
     border: '1px solid #ced4da',
     boxShadow: '0 0 5px 1px rgba(0, 0, 0, 0.1)',
+
   },
+});
+
+const MySubtitle = styled(Typography)({
+  marginLeft: '12px',
+  borderBottom: '1px solid black',
+  paddingBottom: '8px',
+  fontWeight: 'bold',
 });
 
 const Ingresos = () => {
@@ -79,7 +82,7 @@ const Ingresos = () => {
   const [fecha, setFecha] = useState(null);
   const [categoriaCuenta, setCategoriaCuenta] = useState('');
   const [cuenta, setCuenta] = useState('');
-  const [importe, setImporte] = useState(0.0);
+  const [importe, setImporte] = useState('0.00');
   const [conceptoDescripcion, setConceptoDescripcion] = useState('');
 
   const handleFechaChange = (date) => {
@@ -91,44 +94,90 @@ const Ingresos = () => {
     setNumComprobante(numComprobante + 1);
   };
 
+  const handleImporteChange = (e) => {
+    const value = e.target.value;
+    const formattedValue = parseFloat(value).toFixed(2);
+    setImporte(formattedValue);
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <MyContainer>
-      <Typography variant="h3" align="center" gutterBottom style={{borderBottom:'1px solid black'}}>
-        Ingresos
-      </Typography>
-      <MyForm>
-      <MyRow>          
-        <LocalizationProvider >
-            <MyDatePicker
-              label="Fecha"
-              value={fecha}
-              onChange={handleFechaChange}
-              renderInput={(params) => <MyField {...params} size="small" />}
-            />
+      <MyContainer>
+        <Typography variant="h3" align="center" gutterBottom style={{ borderBottom: '1px solid black' }}>
+          Ingresos
+        </Typography>
+        <MyForm>
+          <MyRow>
+            <LocalizationProvider >
+              <MyDatePicker
+                label="Fecha"
+                value={fecha}
+                onChange={handleFechaChange}
+                renderInput={(params) => <MyField {...params} size="small" />}
+              />
             </LocalizationProvider>
-          <MyField size="medium" label="Número de comprobante" value={numComprobante} style={{marginLeft: '80px'}} />
-        </MyRow>
-        <MyField size="small" label="Categoría cuenta" style={{marginLeft: '0px'}} />
-        <MyField size="small" label="Cuenta" style={{marginLeft: '0px'}} />
-        <MyField size="small" label="Importe" style={{marginLeft: '0px'}} />
-        <MyField size="small" label="Concepto/Descripción" style={{marginLeft: '0px'}} />
-        <MyField size="small" label="Categoría cuenta" style={{marginLeft: '0px'}} />
-        <MyField size="small" label="Cuenta" style={{marginLeft: '0px'}} />
-        <MyField size="small" label="Importe" style={{marginLeft: '0px'}} />
-        <MyField size="small" label="Concepto/Descripción" style={{marginLeft: '0px'}} />
-        <MyButtonRow>
-          <MySubmitButton variant="contained" color="success">
-            Guardar
-          </MySubmitButton>
-          <Button variant="contained" color="secondary">
-            Cancelar
-          </Button>
-        </MyButtonRow>
-     
+            <MyField size="medium" label="Número de comprobante" value={numComprobante} style={{ marginLeft: '80px' }}
+              disabled={true}
+            />
 
-      </MyForm>
-    </MyContainer>
+
+          </MyRow>
+          <MySubtitle variant="h5">Detalle del Ingreso</MySubtitle>
+          {/* <MyField size="small" label="Categoría cuenta" style={{marginLeft: '0px'}} /> */}
+          <MyField
+            size="small"
+            label="Categoría cuenta"
+            style={{ marginLeft: '0px' }}
+            value={categoriaCuenta}
+            onChange={(e) => setCategoriaCuenta(e.target.value)}
+            required
+            select
+            SelectProps={{ native: true }}
+            InputLabelProps={{ shrink: true }}
+          >
+            <option value="" disabled>Selecciona una categoria</option>
+            <option value="opcion1">Disponibilidades</option>
+            <option value="opcion2">Opción 2</option>
+            <option value="opcion3">Opción 3</option>
+          </MyField>
+          {/* <MyField size="small" label="Cuenta" style={{ marginLeft: '0px' }} /> */}
+          <MyField
+            size="small"
+            label="Cuenta"
+            style={{ marginLeft: '0px' }}
+            value={cuenta}
+            onChange={(e) => setCuenta(e.target.value)}
+            required
+            select
+            SelectProps={{ native: true }}
+            InputLabelProps={{ shrink: true }}
+          >
+            <option value="" disabled>Seleccione una cuenta</option>
+            <option value="opcion1">Banco Frances</option>
+            <option value="opcion2">Banco Galicia</option>
+            <option value="opcion3">Caja</option>
+          </MyField>
+          {/* <MyField size="small" label="Importe" style={{ marginLeft: '0px' }} /> */}
+          <MyField size="small" label="Importe" type="number" 
+            inputMode="decimal"step="0.01"  />
+          <MyField size="small" label="Concepto/Descripción" style={{ marginLeft: '0px' }} />
+          <MySubtitle variant="h5">Origen del Ingreso</MySubtitle>
+          <MyField size="small" label="Categoría cuenta" style={{ marginLeft: '0px' }} />
+          <MyField size="small" label="Cuenta" style={{ marginLeft: '0px' }} />
+          <MyField size="small" label="Importe" style={{ marginLeft: '0px' }} />
+          <MyField size="small" label="Concepto/Descripción" style={{ marginLeft: '0px' }} />
+          <MyButtonRow>
+            <MySubmitButton variant="contained" color="success">
+              Guardar
+            </MySubmitButton>
+            <Button variant="contained" color="secondary">
+              <a href="./registros">Cancelar</a>
+            </Button>
+          </MyButtonRow>
+
+
+        </MyForm>
+      </MyContainer>
     </LocalizationProvider>
   );
 };
